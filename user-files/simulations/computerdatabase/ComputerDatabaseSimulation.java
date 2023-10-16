e438afc79257ee51d6885286a3b778dec2c5fcb9
ManjeetSharma <sharmaboymanjeet12@gmail.com>
2023-10-16 16:56:52 +0530

    commited

--- user-files/simulations/computerdatabase/ComputerDatabaseSimulation.java
+++ user-files/simulations/computerdatabase/ComputerDatabaseSimulation.java
@@ -18,66 +18,68 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ComputerDatabaseSimulation extends Simulation
 {
     public void socket() {
+        for (int i = 0; i < 7; i++) {
 
 
-        HttpProtocolBuilder httpProtocol = http
-                .baseUrl("https://chatqa.clovedental.in")
-                .wsBaseUrl("wss://chatqa.clovedental.in") // WebSocket URL
-                .acceptHeader("text/plain, */*; q=0.01")
-                .header("Sec-WebSocket-Version", "13")
-                .header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits; server_max_window_bits=15")
-                .header("Sec-WebSocket-Key", "amxpZGVmbHh5bGljeXl2aQ==")
-                .header("current_chat_id", "0")
-                .header("userId", "7")
-                .header("Connection", "Upgrade")
-                .header("authToken", "RDZgQ1v6bZ2LyQvy5mObo9PtnrB1fICp7BWAOIaD6a4%3D")
-                .header("deviceId", "93723B60-19AE-4B82-831F-9DD9A4899351-1696586186.616768-12EFBD82-6609-4FBE-A588-B5A75B8C2779")
-                .header("connected_in", "1")
-                .header("Upgrade", "websocket")
-                .header("Host", "chatqa.clovedental.in:443")
-                .header("Origin", "wss://chatqa.clovedental.in")
-                .header("platform", "iOS");
-        HttpProtocolBuilder httpProtocol1 = http
-                .baseUrl("https://chatqa.clovedental.in")
-                .wsBaseUrl("wss://chatqa.clovedental.in") // WebSocket URL
-                .acceptHeader("text/plain, */*; q=0.01")
-                .header("Sec-WebSocket-Version", "13")
-                .header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits; server_max_window_bits=15")
-                .header("Sec-WebSocket-Key", "amxpZGVmbHh5bGljeXl2aQ==")
-                .header("current_chat_id", "0")
-                .header("userId", "7")
-                .header("Connection", "Upgrade")
-                .header("authToken", "RDZgQ1v6bZ2LyQvy5mObo9PtnrB1fICp7BWAOIaD6a4%3D")
-                .header("deviceId", "93723B60-19AE-4B82-831F-9DD9A4899351-1696586186.616768-12EFBD82-6609-4FBE-A588-B5A75B8C2779")
-                .header("connected_in", "1")
-                .header("Upgrade", "websocket")
-                .header("Host", "chatqa.clovedental.in:443")
-                .header("Origin", "wss://chatqa.clovedental.in")
-                .header("platform", "iOS");
+            HttpProtocolBuilder httpProtocol = http
+                    .baseUrl("https://chatqa.clovedental.in")
+                    .wsBaseUrl("wss://chatqa.clovedental.in") // WebSocket URL
+                    .acceptHeader("text/plain, */*; q=0.01")
+                    .header("Sec-WebSocket-Version", "13")
+                    .header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits; server_max_window_bits=15")
+                    .header("Sec-WebSocket-Key", "amxpZGVmbHh5bGljeXl2aQ==")
+                    .header("current_chat_id", "0")
+                    .header("userId", "7")
+                    .header("Connection", "Upgrade")
+                    .header("authToken", "RDZgQ1v6bZ2LyQvy5mObo9PtnrB1fICp7BWAOIaD6a4%3D")
+                    .header("deviceId", "93723B60-19AE-4B82-831F-9DD9A4899351-1696586186.616768-12EFBD82-6609-4FBE-A588-B5A75B8C2779")
+                    .header("connected_in", "1")
+                    .header("Upgrade", "websocket")
+                    .header("Host", "chatqa.clovedental.in:443")
+                    .header("Origin", "wss://chatqa.clovedental.in")
+                    .header("platform", "iOS");
+            HttpProtocolBuilder httpProtocol1 = http
+                    .baseUrl("https://chatqa.clovedental.in")
+                    .wsBaseUrl("wss://chatqa.clovedental.in") // WebSocket URL
+                    .acceptHeader("text/plain, */*; q=0.01")
+                    .header("Sec-WebSocket-Version", "13")
+                    .header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits; server_max_window_bits=15")
+                    .header("Sec-WebSocket-Key", "amxpZGVmbHh5bGljeXl2aQ==")
+                    .header("current_chat_id", "0")
+                    .header("userId", "7")
+                    .header("Connection", "Upgrade")
+                    .header("authToken", "RDZgQ1v6bZ2LyQvy5mObo9PtnrB1fICp7BWAOIaD6a4%3D")
+                    .header("deviceId", "93723B60-19AE-4B82-831F-9DD9A4899351-1696586186.616768-12EFBD82-6609-4FBE-A588-B5A75B8C2779")
+                    .header("connected_in", "1")
+                    .header("Upgrade", "websocket")
+                    .header("Host", "chatqa.clovedental.in:443")
+                    .header("Origin", "wss://chatqa.clovedental.in")
+                    .header("platform", "iOS");
 
-        ScenarioBuilder scn = scenario("WebSocket Load Test")
-                .exec(
-                        ws("WebSocket Connect")
-                                .connect("/wss2/socket")
-                )
-                .pause(10000) // Adjust as needed for your load testing scenario
-                .exec(ws("WebSocket Close").close());
-        ScenarioBuilder scn1 = scenario("WebSocket Load Test 2")
-                .exec(
-                        ws("WebSocket Connect")
-                                .connect("/wss2/socket")
-                )
-                .pause(10000); // Adjust as needed for your load testing scenario
+            ScenarioBuilder scn = scenario("WebSocket Load Test")
+                    .exec(
+                            ws("WebSocket Connect")
+                                    .connect("/wss2/socket")
+                    )
+                    .pause(10000) // Adjust as needed for your load testing scenario
+                    .exec(ws("WebSocket Close").close());
+            ScenarioBuilder scn1 = scenario("WebSocket Load Test 2")
+                    .exec(
+                            ws("WebSocket Connect")
+                                    .connect("/wss2/socket")
+                    )
+                    .pause(10000); // Adjust as needed for your load testing scenario
 //            .exec(ws("WebSocket Close").close());
 
-        {
+            {
 
-            setUp(
-                    scn.injectOpen(atOnceUsers(1)).protocols(httpProtocol)
-            );
+                setUp(
+                        scn.injectOpen(atOnceUsers(1)).protocols(httpProtocol)
+                );
 //        setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
 //        setUp(scn1.injectOpen(atOnceUsers(1))).protocols(httpProtocol1);
 //        setUp(scn.injectOpen(atOnceUsers(1)),scn1.injectOpen(atOnceUsers(1)).protocols(httpProtocol));
+            }
         }
     }
 }
