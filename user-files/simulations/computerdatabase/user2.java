c70995c063bcd12d15002a7fb1fca2d9fb48830e
ManjeetSharma <sharmaboymanjeet12@gmail.com>
2023-11-01 19:48:22 +0530

    commit 100 users

--- user-files/simulations/computerdatabase/user2.java
+++ user-files/simulations/computerdatabase/user2.java
@@ -3,6 +3,7 @@ package computerdatabase;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
+import scala.collection.immutable.List;
 
 import java.util.ResourceBundle;
 
@@ -10,6 +11,7 @@ import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.ws;
+import scala.collection.immutable.Range;
 
 public class user2 extends Simulation {
     String baseUrl = "https://chatqa.clovedental.in";
@@ -18,23 +20,7 @@ public class user2 extends Simulation {
 
     ResourceBundle resourceBundleLoginToken = ResourceBundle.getBundle("SecWebSocketKey");
     private String doctorUser = resourceBundleLoginToken.getString("user1");
-    HttpProtocolBuilder httpProtocol = http
-            .baseUrl(baseUrl)
-            .wsBaseUrl(wsBaseUrl) // WebSocket URL
-            .acceptHeader(acceptHeader)
-            .header("Sec-WebSocket-Version", "13")
-            .header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits; server_max_window_bits=15")
-            .header("Sec-WebSocket-Key", doctorUser)
-            .header("current_chat_id", "0")
-            .header("userId", "7")
-            .header("Connection", "Upgrade")
-            .header("authToken", "RDZgQ1v6bZ2LyQvy5mObo9PtnrB1fICp7BWAOIaD6a4%3D")
-            .header("deviceId", "93723B60-19AE-4B82-831F-9DD9A4899351-1696586186.616768-12EFBD82-6609-4FBE-A588-B5A75B8C2779")
-            .header("connected_in", "1")
-            .header("Upgrade", "websocket")
-            .header("Host", "chatqa.clovedental.in:443")
-            .header("Origin", "wss://chatqa.clovedental.in")
-            .header("platform", "iOS");
+
     HttpProtocolBuilder httpProtocol1 = http
             .baseUrl("https://chatqa.clovedental.in")
             .wsBaseUrl("wss://chatqa.clovedental.in") // WebSocket URL
@@ -73,10 +59,7 @@ public class user2 extends Simulation {
 
     {
 
-        setUp(
-                scn.injectOpen(atOnceUsers(1)).protocols(httpProtocol),
-                scn1.injectOpen(atOnceUsers(1)).protocols(httpProtocol1)
-        );
+
 //        setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
 //        setUp(scn1.injectOpen(atOnceUsers(1))).protocols(httpProtocol1);
 //        setUp(scn.injectOpen(atOnceUsers(1)),scn1.injectOpen(atOnceUsers(1)).protocols(httpProtocol));
